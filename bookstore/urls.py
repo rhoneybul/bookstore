f069e3fb8c4164008dbb377e80c34acac90bc792
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2016-12-20 14:27:36 +0800

    Added Migrations

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,9 +1,10 @@
 
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from store.views import index, store
 
 urlpatterns = [
-	url(r'^store/',store,name = 'store'),
+	url(r'^$',index,name = "homepage"),
+	url(r'^store/',include('store.urls'),name = 'store'),
     url(r'^admin/', admin.site.urls),
 ]
